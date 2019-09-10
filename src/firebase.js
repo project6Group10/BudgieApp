3cb37a812e490d8fa3ff8403fbf2f0a63c7d9a6b
Theodore Belo <theo_b@me.com>
2019-09-10 18:03:45 -0400

    Initial Commit, added firebase.js, and installed node-sass

---
+++ src/firebase.js
@@ -0,0 +1,16 @@
+import firebase from 'firebase';
+
+// Your web app's Firebase configuration
+var firebaseConfig = {
+    apiKey: "AIzaSyAhF0V9ORBoyKJv5ZnzPRMTkC_gbCWJSCY",
+    authDomain: "budgie-app.firebaseapp.com",
+    databaseURL: "https://budgie-app.firebaseio.com",
+    projectId: "budgie-app",
+    storageBucket: "",
+    messagingSenderId: "541631476929",
+    appId: "1:541631476929:web:a7ca3c5f4bbd2d62f59040"
+};
+// Initialize Firebase
+firebase.initializeApp(firebaseConfig);
+
+export default firebase;
