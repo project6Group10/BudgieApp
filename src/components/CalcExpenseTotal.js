32fc360cea227cf4a9d886353b935da4ae973905
Theodore Belo <theo_b@me.com>
2019-09-12 19:04:26 -0400

    pulled expenses from firebase

--- src/components/CalcExpenseTotal.js
+++ src/components/CalcExpenseTotal.js
@@ -1,7 +1,40 @@
-import React from 'react';
+import React, { Component } from 'react';
+import firebase from '../firebase'
+class CalcExpenseTotal extends Component {
+    constructor() {
+        super();
+        this.state = {
+            expenseArray: [],
+            expenseTotal: [],
+        }
+    }
 
-const CalcExpenseTotal = () => {
-    return (
-        <div></div>
-    )
+    componentDidMount() {
+        const expRef = firebase.database().ref('expenseItems').orderByChild('expenseAmount')
+        expRef.on('value', expenses => {
+            this.setState({
+                expenseArray: expenses.val(),
+            })
+        })
+    }
+
+    componentDidUpdate() {
+        console.log(this.state.expenseArray);
+        for (let key in this.state.expenseArray) {
+            console.log(key)
+        }
+    }
+    
+
+    render() {
+        return (
+            <div>
+                <p>Total</p>
+                <span>Ammount</span>
+
+            </div>
+        )
+    }
 }
+
+export default CalcExpenseTotal;
