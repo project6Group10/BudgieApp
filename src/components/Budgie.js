728b952b45f838836afd65570a9893c6fab11aa0
Vanna Chan <vanna.th.chan@gmail.com>
2019-09-15 15:11:00 -0400

    merged Marli's branch.

--- src/components/Budgie.js
+++ src/components/Budgie.js
@@ -3,26 +3,37 @@ import '../sass/App.scss';
 import { Link } from 'react-router-dom';
 import DisplayExpenses from './DisplayExpenses';
 import InputPayday from './InputPayday';
-import Footer from './Footer';
+import BudgieLogo from '../assets/budgie.png';
 import CalcExpenseTotal from './CalcExpenseTotal';
 // import CalcPayday from './components/CalcPayday';
 
 class Budgie extends Component {
     state = {  }
     render() { 
-      return ( 
-          <main>
-              <Link to="/">Home</Link>
-              <Link to="/About">About</Link>
-              <CalcExpenseTotal />
-              <div className="Container">
-                  <InputPayday />
-                  <DisplayExpenses />
-              </div>
-          </main>
-       );
+        return ( 
+            <section className="Container">
+                <nav className="navigation">
+                    <ul className="navigation__list">
+                        <li className="navigation__item">
+                            <Link to="/" className="btn-logo">
+                                <img src={BudgieLogo} className="logo" alt="Budgie Birb Logo."/>    
+                            </Link>
+                        </li>
+                        <li className="navigation__item">
+                            <Link to="/About" className="btn-about">
+                                about
+                            </Link> 
+                        </li>
+                    </ul>
+                </nav>
+                <section>
+                    <CalcExpenseTotal />
+                </section>
+                <section className="flex">
+                    <InputPayday />
+                    <DisplayExpenses />
+                </section>
+            </section>
+        );
     }
-  }
-   
-  export default Budgie;
-  
+}
