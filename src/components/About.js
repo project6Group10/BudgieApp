57d27d5cb3bcad687ea7cf98a368877d43bce979
Marli Dunker <marlidunker@gmail.com>
2019-09-17 11:17:04 -0400

    about page description

--- src/components/About.js
+++ src/components/About.js
@@ -24,17 +24,17 @@ class About extends Component {
                 </nav>
 
                 <main className="Container">
-                    <h1 className="title">When it comes to your money, 
-                        <span className="title__span">don't budge.</span>
+                    <h1 className="title">Need a short-term budget? 
+                        {/* <span className="title__span">Budgie is here for you.</span> */}
                     </h1>
                     <div className="row">
                         <section className="col-1-of-2">
-                            <h2 className="subtitle">who we are</h2>
+                            <h2 className="subtitle">Budgie is here for you</h2>
                             <div className="composition">
                                 <img src={Avatar} alt="Two faceless animated people" className="image composition__photo" />
                             </div>
                             <p className="paragraph">
-                            Lorem ipsum dolor sit amet, consectetur adipiscing elit. Mauris ac eros eget purus molestie rhoncus eu quis mi. Proin faucibus ullamcorper interdum. Donec mattis pulvinar mauris, eu euismod purus placerat vitae. Curabitur eget augue sed dui aliquam consectetur. Vivamus eu elit ac nisl imperdiet pharetra. Fusce lacinia laoreet nunc, ut blandit dui vehicula
+                            Enter the amount and date of your next pay date and Budgie will calculate a daily budget between now and payday. As daily expenses are inccured, enter them into the expense field and Budgie will account for the expenses and adjust the daily budget.
                             </p>
                             <Link to="/Budgie" className="btn-start">start</Link>
                         </section>
